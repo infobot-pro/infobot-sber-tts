489a247db0e06adbd8b089359fea2e9c1d86e84e
Aleksey Kudakov <cto@infobot.pro>
2022-11-27 19:44:24 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# infobot-sber-tts
+# infobot-sber-stt
+Node.JS library for [Sber SmartSpeech](https://developers.sber.ru/portal/tools/smartspeech) service.
+Library can synchronously synthesize speech from text.
+
+To work with this library you need to obtain from Sber SmartSpeech:
+* Client ID
+* Client Secret
+
+Please check [this page](https://developers.sber.ru/portal/tools/smartspeech) for information about credentials.
+
+## Audio file synthesize example:
+```javascript
+var SberTTS = require('infobot-sber-tts');
+var fs = require('fs');
+
+var clientID = '*';
+var clientSecret = '*';
+
+var tts = new SberTTS(clientID, clientSecret);
+tts.generateAudio('Привет, мир!', {
+    format: 'opus',
+    voice: 'Nec_24000'
+}).then(res => {
+    fs.writeFileSync('test.opus', res);
+}).catch(err => console.log(err));
+````
+
+Provided by [INFOBOT LLC.](https://infobot.pro) under Apache 2.0 license.
+
---
+++ index.js
@@ -0,0 +1,89 @@
+const {v4: uuidv4} = require('uuid');
+const axios = require("axios");
+const qs = require('querystring');
+const https = require('https');
+const fs = require('fs');
+
+class InfobotSberTTS {
+    constructor(clientId, clientSecret) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.token = null;
+
+        this.httpsAgent = new https.Agent({
+            ca: [
+                fs.readFileSync('./certs/russian_trusted_root_ca.cer'),
+                fs.readFileSync('./certs/russian_trusted_sub_ca.cer')
+            ], keepAlive: false
+        });
+
+        if (!this.clientId) throw new Error('No Service Client ID provided');
+        if (!this.clientSecret) throw new Error('No Service Client Secret provided');
+    }
+
+    generateToken() {
+        const self = this;
+        return new Promise(function (resolve, reject) {
+            if (!(self.token && self.tokenExpire && self.tokenExpire < (new Date().getTime() - 10 * 1000))) {
+                const payload = {
+                    'scope': 'SBER_SPEECH'
+                };
+                const code = Buffer.from(`${self.clientId}:${self.clientSecret}`).toString('base64');
+                axios({
+                    method: 'post',
+                    url: 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth',
+                    headers: {
+                        'RqUID': uuidv4(),
+                        'Authorization': `Basic ${code}`,
+                    },
+                    data: qs.encode(payload),
+                    httpsAgent: self.httpsAgent
+                }).then(result => {
+                    self.token = result.data.access_token;
+                    self.tokenExpire = result.data.expires_at;
+                    resolve(self.token);
+                }).catch(e => {
+                    reject(e);
+                })
+            } else {
+                resolve(self.token);
+            }
+        });
+    }
+
+    generateAudio(text, params) {
+        const self = this;
+        return new Promise(function (resolve, reject) {
+            if (!text) reject('No text specified');
+            self.generateToken().then(function () {
+                params.format = params.format || 'opus';
+                params.voice = params.voice || 'Nec_24000';
+
+                const data = {
+                    voice: params.voice,
+                    format: params.format,
+                };
+                axios({
+                    method: 'post',
+                    url: 'https://smartspeech.sber.ru/rest/v1/text:synthesize',
+                    headers: {
+                        'Authorization': 'Bearer ' + self.token,
+                        'Content-Type': params.ssml ? 'application/ssml' : 'application/text'
+                    },
+                    params: data,
+                    data: text,
+                    responseType: 'arraybuffer',
+                    httpsAgent: self.httpsAgent
+                }).then(result => {
+                    resolve(result.data);
+                }).catch(e => {
+                    reject(e);
+                })
+            }).catch(function (err) {
+                reject(err);
+            });
+        });
+    }
+}
+
+module.exports = InfobotSberTTS;
